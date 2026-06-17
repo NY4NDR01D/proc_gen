563f0064fedb24ec9c96d3a576581312a11ee268
nyanndroid <nyanndroid@gmail.com>
2026-06-17 18:50:00 +0100

    commit 2

---
+++ DungeonGen.cs
@@ -0,0 +1,27 @@
+using Godot;
+using System;
+
+public partial class DungeonGen : Node3D
+{
+	[ExportGroup("BorderProperties")]
+	[Export]
+	private int border_width; // x
+	[Export]
+	private int border_height; // z
+	[Export]
+	private int border_depth; // y
+	[ExportGroup("RoomProperties")]
+	[Export]
+	private int min_num_rooms;
+	[Export]
+	private int max_num_rooms;
+
+	
+	public override void _Ready()
+	{
+	}
+
+	public override void _Process(double delta)
+	{
+	}
+}
